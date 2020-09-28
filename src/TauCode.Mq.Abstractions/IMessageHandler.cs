e3bc6f27320b0fc482e7b33f24580c290d9829f2
Andrii Kovalenko <ankovalenko@deloitte.com>
2020-09-28 13:42:51 +0300

    Updating.

--- src/TauCode.Mq.Abstractions/IMessageHandler.cs
+++ src/TauCode.Mq.Abstractions/IMessageHandler.cs
@@ -1,7 +1,10 @@
-﻿namespace TauCode.Mq.Abstractions
+﻿using System.Threading.Tasks;
+
+namespace TauCode.Mq.Abstractions
 {
     public interface IMessageHandler
     {
         void Handle(object message);
+        Task HandleAsync(object message);
     }
 }
--- src/TauCode.Mq.Abstractions/IMessageHandlerT.cs
+++ src/TauCode.Mq.Abstractions/IMessageHandlerT.cs
@@ -1,8 +1,12 @@
-﻿namespace TauCode.Mq.Abstractions
+﻿using System.Threading.Tasks;
+
+namespace TauCode.Mq.Abstractions
 {
     public interface IMessageHandler<in TMessage> : IMessageHandler
         where TMessage : IMessage
     {
         void Handle(TMessage message);
+
+        Task HandleAsync(TMessage message);
     }
 }
